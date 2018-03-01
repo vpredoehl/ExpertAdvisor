a156d58065492f99ffc298fa86ab586b721eaa70
Vincent Predoehl <vpredoehl@gmail.com>
2018-02-28 23:10:42 -0600

    reads time field

--- ExpertAdvisor/main.cpp
+++ ExpertAdvisor/main.cpp
@@ -8,14 +8,33 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+#include <chrono>
 
-using std::string;
+using namespace std::chrono;
+using tp = time_point<system_clock, seconds>;
 
+
+std::istream& operator>>(std::istream& i, struct tm &t)
+{
+    i >> std::get_time(&t, "%F %R:%S");
+    return i;
+}
 
 int main(int argc, const char * argv[]) {
     std::ifstream csv { "COR_USD_Week3.csv", std::ios_base::in };
     std::string l;
+    struct tm t;
     
+    csv >> l;   // header line
+    
+    std::getline(csv, l, ',');
+    std::getline(csv, l, ',');
+    std::getline(csv, l, ',');
+    csv >> t;
+    std::cout << std::put_time(&t, "%Y:%m %R");
+    std::getline(csv, l, ',');
+
     csv >> l;
     
     return 0;
